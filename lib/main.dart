8c2a321dac13e930ffd10007fa2aec4180caa595
nguyenductho89 <nguyenductho89@gmail.com>
2021-01-13 22:25:22 +0700

    start flutter

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BullsEyeApp());
+
+class BullsEyeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BullsEye',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: GamePage(title: 'BullsEye'),
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  GamePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to my first app!',
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
+            ),
+            FlatButton(
+              child: Text('Hit Me!', style: TextStyle(color: Colors.blue)),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
